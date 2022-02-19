531b9972a851da871625517b8777738b37ffc1f0
EmissarKir <61598599+EmissarKir@users.noreply.github.com>
2022-02-19 22:25:46 +0300

    install doker

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 const config = require("config");
 const cors = require("cors");
+const path = require("path");
 const initDatabase = require("./startUp/initDatabase");
 const routes = require("./routes");
 
@@ -19,6 +20,16 @@ app.use(cors());
 app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+
+  const indexPath = path.join(__dirname, "client", "index.html");
+
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
