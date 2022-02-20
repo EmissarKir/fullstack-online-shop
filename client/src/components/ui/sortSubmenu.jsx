41f18d93b5cbe7fcf0b11eb51e3a3db853b01975
EmissarKir <61598599+EmissarKir@users.noreply.github.com>
2022-02-20 11:32:33 +0300

    move RadioField to ui

--- client/src/components/ui/sortSubmenu.jsx
+++ client/src/components/ui/sortSubmenu.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import RadioField from "../common/form/radioField";
+import RadioField from "./radioField";
 import { useDispatch } from "react-redux";
 import { switchSortBy } from "../../store/filters";
 
